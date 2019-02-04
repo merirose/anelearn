dfbeb6db6df81b2b042aa357ced64e41f79cf7c8
merirose <45115683+merirose@users.noreply.github.com>
2019-02-05 00:12:07 +0800

    Add files via upload

--- abilms/index.php
+++ abilms/index.php
@@ -2,52 +2,22 @@
 include "connection.php";
 
 ?>
+<?php
+include "header.php";
+?>
 
+<!-- 		<div class="row">
+			<div class="col-lg-3 col-md-3" style="padding-left:0px">
+				<ul class="nav nav-pills nav-stacked">
+				  <li class="active"><a href="#">Home</a></li>
+				  <li><a href="#">Menu 1</a></li>
+				  <li><a href="#">Menu 2</a></li>
+				  <li><a href="#">Menu 3</a></li>
+				</ul>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Learn</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/custom.css">
-  <script src="js/jquery-1.12.1.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-
-</head>
-<body>
-	<div class="container">
-		<div class="row" style="display:flex;align-items:center;">
-			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-				<img src="images/logo.png" class="img-responsive" />
-
-			</div>
-			<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12" style="text-align:right;">
-				Welcome to Learn!
-
-			</div>
-		</div>
-		<div class="row">
-			<nav class="navbar navbar-default">
-			  <div class="container-fluid">
-			    <ul class="nav navbar-nav">
-			      <li class="active"><a href="index.php">Home</a></li>
-			      <?php 
-			      $qry_top=mysqli_query($con,"select * from table_top_category order by top_category_order asc");
-			      while ($rt=mysqli_fetch_array($qry_top)) {
-			      	echo ' <li><a href="#">'.$rt["top_category_name"].'</a></li>';
-			      }
-
-			      ?>
-			    </ul>
-			  </div>
-			</nav>
-		</div>
-		<div class="row">
-
-		</div>
+		</div> -->
 	</div>
-
+	<?php include "footer.php";?>
+	</div>
 </body>
 </html>
